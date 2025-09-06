cbe4eae6f35ec9b3fcd795946ca0e3445f1135a5
Bryan Becerra <87731894+Design-BAB@users.noreply.github.com>
2025-09-05 22:45:11 -0400

    Fixed loop

--- DiceRoll/index.js
+++ DiceRoll/index.js
@@ -1,6 +1,10 @@
 //name of site
 document.getElementById("myH1").textContent = 'Roll the Die!';
 
+//need to get the selector
+const select = document.getElementById("mySelect");
+
+
 //the function of the rolling of the die
 function roll() {
     //first the variables
@@ -9,18 +13,21 @@ function roll() {
     //we gotta have atleast 1 die
     die = Math.floor(Math.random() * 6);
     die++;
-    img = '<img src="' + die + 'Die.bmp" alt="' + die + '">';
+    img = '</br></br><img src="' + die + 'Die.bmp" alt="' + die + '">';
+    //After adding 1 die, see what kindof dice user wants
+    let value = parseInt(select.value) - 1;
     //Now we add the extra dice.
-    for (let i = 1; i < 2; i++) {
+    for (let i = 0; i < value; i++) {
         die = Math.floor(Math.random() * 6);
         die++;
-        img = img + '<img src="' + die + 'Die.bmp" alt="' + die + '">';
+        img = img + '<img src="' + die + 'Die.bmp" alt="' + die + '"> ';
     }
+    //return image
     return img;
 }
 
-
+//set function for the button
 document.getElementById("rollIt").onclick = function(){
-	//console.log(roll());
+    //we roll the dice then we are replacing the innrHTML
 	document.getElementById("result").innerHTML = roll();
 	}
